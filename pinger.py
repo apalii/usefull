ba0dc2ff257363aad0d95c7ac9c251eb877c63a7
Andrii Palii <Andrii.Palii@portaone.com>
2014-10-31 17:46:09 +0200

    simple pinger

---
+++ pinger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+''' Very simple pinger on Python  +  bash
+from day by day routine'''
+
+import ping, socket,time
+from datetime import datetime
+from datetime import date
+
+try:
+    print date.today(),'-',datetime.time(datetime.now())
+    ping.verbose_ping('web.centracom.co.za', count=1)
+except socket.error, e:
+    print "Ping Error:", e
+
+#-------------------------------------------------------
+
+#!/bin/bash
+while [ True ]
+do
+    sudo python pinger.py
+    sleep 5
+done
