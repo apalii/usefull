29214c26fbd272ebc6044942a2cbd3171d3531d8
Andrii Palii <Andrii.Palii@portaone.com>
2015-01-06 17:11:22 +0200

    watcher improove

---
+++ vk_watcher.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+try :
+    from urllib.request import urlopen
+except:
+    from urllib2 import urlopen
+import sys
+import json
+from time import sleep, ctime
+
+class Watcher(object):
+    '''
+    vk watcher class 
+    python 2 and 3 compatible
+    json response example :
+    {"response":[{"uid":1,"first_name":"Pavel","last_name":"Durov","online":0,"last_seen":{"time":1398447188,"platform":7}}]}
+    '''
+    def __init__(self, vk_id, timeout=10, reps=5):
+        self.vk_id = vk_id
+        self.timeout = timeout 
+        self.reps = reps
+        self.url = 'https://api.vk.com/method/users.get?user_ids=' + \
+                    self.vk_id + '&fields=online,last_seen&lang=en'
+
+    def watch(self):
+        '''watch method'''
+        while self.reps !=0:
+            self.reps -= 1
+            page = urlopen(self.url)
+            data = json.load(page)
+            if 'error' in data:
+                print("\nIt seems that such id as :", self.vk_id, " doesn't exist!")
+                sys.exit(1)
+            for i in data['response']:
+                if 'deactivated' in i:
+                    print("\nIt seems that such id as :" + self.vk_id + " has been deleted")
+                    sys.exit(1)
+                elif i['online'] == 1:
+                    print(i['first_name'],i['last_name'] + '\tis ONLiNE\n')
+                else:
+                    print('{} {}\t{}'.format(i['first_name'],i['last_name'], Watcher.devtime(data)))
+            sleep(self.timeout)
+        print('finished')
+
+
+    @staticmethod
+    def devtime(response):
+        dev = 'pc' if response['response'][0]['last_seen']['platform'] == 7 else 'mob'
+        time =  ctime(response['response'][0]['last_seen']['time'])
+        return 'last seen: {} | device: {}'.format(time, dev)
+
+if __name__ == "__main__":
+
+    mywatcher = Watcher('durov',5,2).watch()
