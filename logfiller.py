d5274d33caf3bf11cf95cd3f1ae7479bf903502b
Andrew Palii <dezz.ua@gmail.com>
2016-08-29 18:11:37 +0300

    adding redis

--- logfiller.py
+++ logfiller.py
@@ -16,13 +16,22 @@ idm_host_src='VirtualUSMAllInOne' idm_host_dst='VirtualUSMAllInOne' idm_mac_src=
 idm_mac_dst='00:0C:29:6A:10:93' device='192.168.87.191'/>
 """
 
+import time
+import datetime
+import sys
 import socket
 import random
-import time
-import sys
-import datetime
+import redis
 
 from calendar import monthrange
+
+now = datetime.datetime.now()
+month = now.month
+year = now.year
+last_day = monthrange(year, month)[1]
+start = datetime.datetime(year, month, 1)
+end = datetime.datetime(year, month, last_day)
+start, end = int(start.strftime("%s")), int(end.strftime("%s"))
 
 
 def rand_date():
@@ -30,21 +39,11 @@ def rand_date():
     Returns random date(within current month) in unix time
     :return: int
     """
-    now = datetime.datetime.now()
-    month = now.month
-    year = now.year
-    last_day = monthrange(year, month)[1]
-    start = datetime.datetime(year, month, 1)
-    end = datetime.datetime(year, month, last_day)
-    return random.randrange(int(start.strftime("%s")), int(end.strftime("%s")))
+    return random.randrange(start, end)
 
 
 def rand_plugin():
-    return random.randrange(7001, 7100)
-
-
-def rand_ip():
-    return "192.168.87.{}".format(random.randrange(2, 253))
+    return random.randrange(7001, 7050)
 
 
 class LogFiller(object):
@@ -69,28 +68,44 @@ class LogFiller(object):
         self.host = (self.ip, self.port)
         self.debug = debug
 
-    def create_tcp_connection_and_send(self):
+    def through_mongodb_connection(self):
+        pass
+
+    def through_redis_connection(self):
+        instance = redis.Redis()
+        key = 'logstash:cache:test'
+        for i in xrange(1, self.number_of_lines + 1):
+            instance.lpush(key, self.generate_line())
+
+    def through_tcp_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
         try:
             sock.connect(self.host)
         except socket.timeout as err:
             sock.close()
-            err_msg = "There is connection problem : {}\nPlease check iptables rules or logstash conf".format(err)
+            err_msg = "Please check iptables rules or logstash conf: {}".format(err)
+            sys.exit(err_msg)
+        except socket.error as err:
+            sock.close()
+            err_msg = "Please check iptables rules: {}".format(err)
             sys.exit(err_msg)
 
         for i in xrange(1, self.number_of_lines + 1):
             line_to_send = self.generate_line()
             if self.debug:
                 print line_to_send
-            sock.send(line_to_send)
+            try:
+                sock.sendall(line_to_send)
+            except socket.error as err:
+                print("Unable to send a msg : {}".format(err))
+            time.sleep(0.01)
         sock.close()
 
     @staticmethod
     def generate_line():
         data_field = """AV - Alert - "1470314952" --> RID: "5502"; RL: "3"; RG: "pam,syslog,"; RC: "Login session closed."; USER: "None"; SRCIP: "None"; HOSTNAME: "VirtualUSMAllInOne"; LOCATION: "/var/log/auth.log"; EVENT: "[INIT]Aug  4 08:49:11 VirtualUSMAllInOne sudo: pam_unix(sudo:session): session closed for user root[END]";"""
         line = "<entry id='2882' v='2' fdate='2016-08-04 12:49:12' date='{1}' plugin_id='{2}' " \
-               "sensor='192.168.87.191' src_ip='{3}' dst_ip='{3}' src_port='0' dst_port='0' " \
+               "sensor='192.168.87.191' src_ip='192.168.87.{3}' dst_ip='192.168.87.{3}' src_port='0' dst_port='0' " \
                "tzone='-4.00' datalen='307' data='{0}' plugin_sid='5502' proto='6'  " \
                "ctx='cad377ce-4772-11e6-a4c1-000c296a1093' " \
                "src_host='cb973a55-4772-11e6-a4c1-000c296a1093' dst_host='cb973a55-4772-11e6-a4c1-000c296a1093' " \
@@ -98,11 +113,10 @@ class LogFiller(object):
                "username='root' userdata1='/var/log/auth.log' userdata2='Login session closed.' " \
                "userdata3='pam,syslog,' userdata4='none' idm_host_src='VirtualUSMAllInOne' " \
                "idm_host_dst='VirtualUSMAllInOne' idm_mac_src='00:0C:29:6A:10:93' idm_mac_dst='00:0C:29:6A:10:93' " \
-               "device='192.168.87.191'/>\n".format(
-            data_field, rand_date(), rand_plugin(), rand_ip()
-        )
+               "device='192.168.87.191'/>\n".format(data_field, rand_date(), rand_plugin(), random.randrange(2, 99))
 
         return line
 
-test = LogFiller(lines=3, debug=True)
-test.create_tcp_connection_and_send()
+test = LogFiller(lines=100000, ip='192.168.100.243', debug=False)
+#test.through_tcp_socket()
+test.through_redis_connection()
